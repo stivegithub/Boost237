a18e004b0ee6ec4afa491973ccab4b47219ce65a
stive <fossistive627@gmail.com>
2023-10-15 00:12:18 +0100

    second commit

--- src/components/LoginLeft.tsx
+++ src/components/LoginLeft.tsx
@@ -1,11 +1,15 @@
 import React, { FunctionComponent } from 'react'
 import logo from '../images/logo.png'
+import Typewriter from '../helper/Typewriter'
 
 const LoginLeft:FunctionComponent = () => {
+  const text=[' le travail', ' le professionnalisme', " l'efficacite", " le respect des delais", "la rigueur"]
   return (
     <div className=' p-9'>
         <img src={logo} alt="le logo de boost 237 " width={`100`} height={`30`} />
-        <p className=' text-sm'>Marquer votre presence sur les medias sociaux.</p>
+        <p className=' text-sm mb-9 z-10'>Marquer votre presence sur les medias sociaux.</p>
+        <p className=' font-bold text-3xl text-white '>Bienvenue chez nous. Confiez nous votre presence en ligne car </p>
+        <div className=' text-white text-2xl text-center '><Typewriter text={text}  spedd={200}/></div>
         
     </div>
   )
---
+++ src/helper/Typewriter.tsx
@@ -0,0 +1,60 @@
+import { time } from 'console';
+import React, { FunctionComponent, useEffect, useState } from 'react'
+
+type props={
+    text:string[],
+    spedd:number
+}
+
+const Typewriter:FunctionComponent<props> = ({text, spedd=100}) => {
+    const [displayedText, setDisplayedText]= useState<string>('');
+    const [index, setIndex]= useState<number>(0);
+    const [isDeleting, setIsDeleting]=useState<boolean>(false)
+    const [textIndex, setTextIndex]=useState<number>(0)
+
+    useEffect(()=>{
+        let timeoutId:any;
+        if(isDeleting){
+          if(index>0){
+            timeoutId= setTimeout(() => {
+                setDisplayedText((prevText)=>prevText.substring(0, prevText.length -1))
+                setIndex((prevIndex)=>prevIndex - 1)
+                
+            }, spedd);
+
+          }
+          else {
+            setIsDeleting(false)
+            if(textIndex===text.length -1){
+                return setTextIndex(0)
+              }
+          if(textIndex<text.length){
+            return  setTextIndex((prevIndex)=>prevIndex+1)
+          }
+          
+          }
+        }
+        else{
+            if(index<text[textIndex].length){
+                timeoutId= setTimeout(() => {
+                    setDisplayedText((prevText)=> prevText + text[textIndex][index])
+                    setIndex((prevIndex)=>prevIndex+1);
+                }, spedd);
+            }
+            else{
+
+                setTimeout(() => {
+                    setIsDeleting(true)
+                }, 1000);
+            }
+        }
+        return ()=>clearTimeout(timeoutId)
+    },[text, textIndex, index, isDeleting, spedd])
+  return (
+    <div>
+      {displayedText}
+    </div>
+  )
+}
+
+export default Typewriter
